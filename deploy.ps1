cff8255ea35a4c80a2c7f4f5c509da2ab4581091
tayhall <4ndrewhall@gmail.com>
2020-04-17 08:13:25 +0100

    Updated Comments

--- deploy.ps1
+++ deploy.ps1
@@ -1,46 +1,48 @@
 <#
-Default Parameters
+The following script deploys a Windows 2016 datacentre server and joins it to the domain.
+
+Default Parameters below. Set the parameter under initial variables and pass with NewResourceGroup
+ deployment to chnage the defaults i.e. $vmSize = "Standard_D2s_v2" and then use -vmSize $vmSize
 
 Name                          Type                       Value
 ============================  =========================  ==========
-adminUsername                 String                     Andrew
-adminPassword                 SecureString
+vmAdminUsername               String                     Andrew
+domainUsername                String                     Andrew
+vmAdminPassword               SecureString
+domainPassword                SecureString
 domainName                    String                     corp.UKTSTDOM.local
 dnsPrefix                     String                     mywan
 vmSize                        String                     Standard_D2s_v3
 _artifactsLocation            String                     https://raw.githubusercontent.com/tayhall/Azure-DC/master/azuredeploy.json
 _artifactsLocationSasToken    SecureString
-location                      String                     eastus
-virtualMachineName            String                     adVM
-virtualNetworkName            String                     adVNET
-virtualNetworkAddressRange    String                     10.0.0.0/16
-inboundNatRulesName           String                     adRDPvm
-networkInterfaceName          String                     adNic
+location                      String                     Defaults to resource group location
+virtualMachineName            String                     dnsLabelPrefix
+existingVNETName              String                     adVNET
 privateIPAddress              String                     
-subnetName                    String                     adSubnet
-subnetRange                   String                     10.0.0.0/24
-publicIPAddressName           String                     adPublicIP
-availabilitySetName           String                     adAvailabiltySet
-loadBalancerName              String                     adLoadBalancer
+existingSubnetName            String                     adSubnet
+
 
 #>
 
 # Inital Variables
 
-#$projectName = Read-Host -Prompt "Enter a project name that is used to generate resource group name"
-$location = "eastus"
+# Uncomment to change default settings
+
+# $location = "eastus"
+# $vmSize = "Standard_D2s_v2"
 $adminUsername = "Andrew"
 $adminPassword = Read-Host -Prompt "Enter the admin password" -AsSecureString
 $resourceGroupName = Read-Host -Prompt "Enter Resource Group Name"
-$dnsLabelPrefix = "mbsrv1"
-$existingVNETName = "adVNET"
-$existingSubnetName = "adSubnet"
+$dnsLabelPrefix = "mbsrv2"
+# $existingVNETName = "adVNET"
+# $existingSubnetName = "adSubnet"
 
 $templateUri = "https://raw.githubusercontent.com/tayhall/vmDomJoin/master/azuredeploy.json"
 
 New-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
     -TemplateURI $templateUri `
+    -dnsLabelPrefix $dnsLabelPrefix `
     -domainPassword $adminPassword `
     -vmAdminPassword $adminPassword
     
